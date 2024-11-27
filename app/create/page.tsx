b6c69f3e9120995a3c2ef00a663163d8fdeed9c1
vishesh-sachan <visheshsachan021@gmail.com>
2024-11-27 15:18:27 +0530

    added create page and modified home page

---
+++ app/create/page.tsx
@@ -0,0 +1,11 @@
+import QrGenerator from "@/components/QrGenerator";
+
+
+export default function Create() {
+  return (
+    <div>
+      <h1>Create QR Code</h1>
+      <QrGenerator />
+    </div>
+  );
+}
